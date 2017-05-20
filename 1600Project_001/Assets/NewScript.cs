ebd9b19cc61c2320cb4afb3cbcdb58ee12791037
Spencer Barber <spencercbarber@gmail.com>
2017-05-19 20:27:39 -0600

    Branching and Merging Intro
    Ok, I created a sub heading so practice branching and merging in.. here goes! :)

--- 1600Project_001/Assets/NewScript.cs
+++ 1600Project_001/Assets/NewScript.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class NewScript : MonoBehaviour {
 
-	//Here is a generic comment to prove that I know how to do the following:
-	//Open and Edit a script in Visual Studio
-	//Stage, Commit, and Push a change in GitKraken
+	/*
+	Here is a generic comment to prove that I know how to do the following:
+	Open and Edit a script in Visual Studio
+	Stage, Commit, and Push a change in GitKraken
+	*/
 
+	/* Branching and Merging
+	
+	Today we are going to talk about Bugs.
+	
+
+	*/
 
 }
