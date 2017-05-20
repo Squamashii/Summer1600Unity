247e1bd43c31f1429f60e61a895c7c83e6c4ba7f
Spencer Barber <spencercbarber@gmail.com>
2017-05-19 20:36:48 -0600

    Insects Commit #2
    My recent inset discovery

--- 1600Project_001/Assets/NewScript.cs
+++ 1600Project_001/Assets/NewScript.cs
@@ -15,6 +15,8 @@ public class NewScript : MonoBehaviour {
 	
 	Insects:
 		Insects are the most common bugs you will see. This includes any bug with 3 body parts (Head, thorax, abdomen) and 6 legs.
+		Recent discovery: I just found a short-winged blister beetle for the first time. It actually secreted blister juice when i nudged it with a stick! gross! I'm glad I didn't touch it with my hand.
+
 	*/
 
 }
