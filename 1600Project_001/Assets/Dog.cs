8f0ff6911d754114e10709642392adab4840eb09
Spencer Barber <spencercbarber@gmail.com>
2017-05-22 11:34:33 -0600

    More script changes

---
+++ 1600Project_001/Assets/Dog.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dog : Mammal {
+
+	//Properties
+
+	//Functions
+	//Attack
+	//Wags Tail
+	//Panting
+	//Barking
+}
--- 1600Project_001/Assets/Mammal.cs
+++ 1600Project_001/Assets/Mammal.cs
@@ -2,13 +2,18 @@
 using System.Collections;
 
 public class Mammal : Animal {
-
+	//
 	//Properties
 	//Hair
 	public bool hasHair = true;
 	public Color hairColor = Color.grey;
-
+	//
+	//Warm Blooded
 	//HowManyLegs
 	public int legCount = 1;
 
+	//FUNCTIONS
+	//Live Birth Overides Reproduction
+	//Feed Milk
+	//Bite
 }
