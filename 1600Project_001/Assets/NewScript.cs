a4b27f9e64ad43dcd6c2f3c89630a8e6ac7403ed
Spencer Barber <spencercbarber@gmail.com>
2017-05-22 20:27:53 -0600

    Merge branch 'develop'

--- 1600Project_001/Assets/NewScript.cs
+++ 1600Project_001/Assets/NewScript.cs
@@ -6,7 +6,12 @@ public class NewScript : MonoBehaviour {
 	//Well, here is going to be my first commit of branch 1
 	/* AAAAAAAAND here is commit #2
 	 * I added this NewScript to the unity assets folder on the first commit, and then I wrote this comment on the second one.
+	
+	Well that was interesting, the 2nd branch was completely disintegrated upon merging..
+	 * 
 	 */
+
+
 //Ok, something strange happened during the 2nd branch commits. Here is branch 3 commit 1
 //Here is a second commit for branch 3, hallelujah!
 }
