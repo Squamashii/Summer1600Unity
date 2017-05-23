fcb155b9463fdc0b32c864af5bf7af5180f78225
Spencer Barber <spencercbarber@gmail.com>
2017-05-22 20:39:52 -0600

    F2 B1 C1 Updated

--- 1600Project_001/Assets/NewScript.cs
+++ 1600Project_001/Assets/NewScript.cs
@@ -15,4 +15,5 @@ public class NewScript : MonoBehaviour {
 
 	//I might as well write some real infomration in here. Penguins can fly.
 				//Update: Penguins can't fly
+		//I just realized that I was editing the feature and not the branch.. grr
 }
