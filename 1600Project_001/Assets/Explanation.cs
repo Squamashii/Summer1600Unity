520fd97b4705ce64241649358162a53489fd19c4
Spencer Barber <spencercbarber@gmail.com>
2017-05-22 19:40:52 -0600

    Commenting on Variables
    Created an "explanation" C# script where I wrote about variables.. and the other scripts contain the variables we went over in class

---
+++ 1600Project_001/Assets/Explanation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explanation : MonoBehaviour {
+
+	//What is a variable?
+	/*
+	 * A variable is a bit of information that is stored in the program. 
+	 * Variables can be whole numbers (int), decimal numbers (float), true or false (bool), words/sentences (string), etc.
+	 * To use a variable in your code, you have to first declare it by stating the variable type, 
+	*/
+	public int wholeNumber = 10;
+	public float decimalNumber = 5.5f;
+	public string[] sentences = { "This is a string variable" "This is another string unput"};
+	public Color testColor = Color.black;
+	public bool trueOrFalse = true;
+}
