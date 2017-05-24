391a5445d2e3313315dd11e5d1d4ffa589bcb0c5
Spencer Barber <spencercbarber@gmail.com>
2017-05-24 17:46:02 -0600

    If Statements assignment
    COMMENTS are in the explanation script, and if statements are in the Animal and Dog scripts

--- 1600Project_001/Assets/Explanation.cs
+++ 1600Project_001/Assets/Explanation.cs
@@ -8,10 +8,34 @@ public class Explanation : MonoBehaviour {
 	 * A variable is a bit of information that is stored in the program. 
 	 * Variables can be whole numbers (int), decimal numbers (float), true or false (bool), words/sentences (string), etc.
 	 * To use a variable in your code, you have to first declare it by stating the variable type, 
-	*/
-	public int wholeNumber = 10;
+	*
+	
 	public float decimalNumber = 5.5f;
-	public string[] sentences = { "This is a string variable" + "This is another string output"};
+	public string[] sentences = { "This is a string variable"};
 	public Color testColor = Color.black;
 	public bool trueOrFalse = true;
+
+	//IF / Else / Else IF statements
+	* All if else statements must start with a call to action such as: void Start (the Start has to be capitalized. I had to figure that out.
+	 * The void part gets parenthesis, and then the IF part gets curly brackets. 
+	 * The comparison symbol is a double equal sign "==". This will compare a variable to a value and test to see if it is true. If it IS true, then the rest of the function will work.
+	 * A boolean variable is inherently false, unless you state otherwise.
+	 * The IF statement function is just searching for truth.. whether that means that you want it to be true or false, whatever you are comparing it to, if it matches, then it is true.
+	 * My other if statements are in the Animal and Dog scripts.
+	 */
+	public string bodyType = "athletic";
+	public int wholeNumber = 10;
+
+	void Start()
+	{
+		if (bodyType == "athletic")
+		{
+			print(this.name + " is faster than you");
+		} 
+		
+		if (wholeNumber == 10)
+		{
+			print("this " + this.name + " is equal to 10");
+		}
+	} 
 }
