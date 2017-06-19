436b9a20c1274e0b8cb05c5e2e157ae9f7dc7748
Spencer Barber <spencercbarber@gmail.com>
2017-06-19 12:22:32 -0600

    Flow Branch 1, Commit 2
    Utah Cons

--- 1600Project_001/Assets/GitFlow.cs
+++ 1600Project_001/Assets/GitFlow.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GitFlow : MonoBehaviour {
 
 	//Utah Pros: Summers are beautiful, close to family, finish school at UVU
+	//Utah Cons: I hate snow, am I too close to family? I don't feel like I'm progressing
 
 
 
