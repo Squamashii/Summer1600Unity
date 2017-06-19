6b902f91597224ff4a82f9b5f77ce64253f99c53
Spencer Barber <spencercbarber@gmail.com>
2017-06-19 12:13:32 -0600

    Scopes and Access Mods
    We made a script to go over scopes and access modifiers in class

---
+++ 1600Project_001/Assets/ScopeAccess.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAccess : MonoBehaviour {
+
+public int coins = 0;
+
+void Attack(){
+
+	void SwordAttack(){
+
+		if(SwordAttack > EnemyDefense){
+			EnemyHealth.AddDamage();
+		}
+		else{
+			EndTurn();
+		}
+	}
+
+	void AxeAttack(){
+		
+		if(AxeAttack > EnemyDefense){
+			EnemyHealth.AddDamage();
+		}
+		else{
+			EndTurn();
+		}
+
+	}
+}
+
+
+
+
+
+}
