78991ab50fa2011fd6a31ada6209ec82d9379f1b
Spencer Barber <spencercbarber@gmail.com>
2017-06-19 12:25:47 -0600

    Flow Branch 2 Commit 1 Hawwaii Pros

--- 1600Project_001/Assets/GitFlow.cs
+++ 1600Project_001/Assets/GitFlow.cs
@@ -6,6 +6,7 @@ public class GitFlow : MonoBehaviour {
 	//Utah Pros: Summers are beautiful, close to family, finish school at UVU
 	//Utah Cons: I hate snow, am I too close to family? I don't feel like I'm progressing
 
+	//Hawaii Pros: Better climate, I could grow my plants, explore for a while, the beach
 
 
 
