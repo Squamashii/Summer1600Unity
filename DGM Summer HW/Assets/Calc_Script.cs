f01f90dcb18a74c1beb129da543a623d441a9b4a
Spencer Barber <spencercbarber@gmail.com>
2017-06-26 13:03:10 -0600

    Unity UI practice
    Preston went over some basic UI stuff in Unity with us

---
+++ DGM Summer HW/Assets/Calc_Script.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Calc_Script : MonoBehaviour {
+
+	public InputField in1;
+	public InputField in2;
+	public Text result;
+	
+		public void AddClick(){
+		float num = float.Parse(in1.text) + float.Parse(in2.text);
+		result.text = num.ToString();
+	}
+
+	public void DivClick(){
+		float num = float.Parse(in1.text) / float.Parse(in2.text);
+		result.text = num.ToString();
+	}
+	
+	public void MultClick(){
+		float num = float.Parse(in1.text) * float.Parse(in2.text);
+		result.text = num.ToString();
+	}
+
+	public void SubClick(){
+		float num = float.Parse(in1.text) - float.Parse(in2.text);
+		result.text = num.ToString();
+	}
+}
